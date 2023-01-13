97bb8aebfa6bc5477ada9c5ac95c8235947999bd
Brian Shanley <bshanley@gmail.com>
2023-01-13 12:00:26 +0000

    pwa simple without manifest link

---
+++ pages/about.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Link from "next/link";
+import Head from "next/head";
+
+<Head>
+  <title>The About Page</title>
+  {/* <!-- Primary Meta Tags --> */}
+  <meta name="title" content="The About Page" />
+  <meta name="description" content="next js pwa testing app." />
+</Head>;
+export default function About() {
+  return <div>About</div>;
+}
